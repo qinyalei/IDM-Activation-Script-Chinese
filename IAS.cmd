ca449eed3036d6bad5c6bf4a08bde6353609a146
jiangbeichen233 <92234617+jiangbeichen233@users.noreply.github.com>
2024-07-15 02:52:41 +0800

    更新 IAS.cmd

--- IAS.cmd
+++ IAS.cmd
@@ -1,4 +1,4 @@
-chcp 936
+chcp 65001
 @set iasver=1.2
 @setlocal DisableDelayedExpansion
 @echo off
@@ -18,7 +18,7 @@ chcp 936
 
 
 
-::  To activate, run the script with "/act" parameter or change 0 to 1 in below line
+::  To activate, run the script with "act" parameter or change 0 to 1 in below line
 set _activate=0
 
 ::  To Freeze the 30 days trial period, run the script with "/frz" parameter or change 0 to 1 in below line
